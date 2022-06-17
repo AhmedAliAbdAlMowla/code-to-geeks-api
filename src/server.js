1535df6fb00cbe746f2337dbb091a4f92562ead5
Ahmed Ali Ahmed <47031477+AhmedAliAbdAlMowla@users.noreply.github.com>
2022-06-17 17:53:48 +0200

    eew

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = require("../src/app/app");
 const server = http.createServer(app);
 const PORT =   process.env.PORT || 4000;
 
+
+
 server.listen(PORT, (err) =>
   console.log(err ? err : `Server started on port ${PORT}..✌️✌️ 🥤`)
 );
